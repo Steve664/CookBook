8b42c1f8ab5098c2fce7ee76953aff50133790c1
Steve Kariuki <stevekariuki5@gmail.com>
2023-03-29 03:18:29 +0300

    relationships

--- app/models/allergen.rb
+++ app/models/allergen.rb
@@ -1,2 +1,6 @@
 class Allergen < ApplicationRecord
+    has_many :recipe_allergens, dependent: :destroy
+    has_many :recipes, through: :recipe_allergens
+  
+    validates :name, presence: true, uniqueness: true
 end
--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,3 +1,10 @@
 class Recipe < ApplicationRecord
   belongs_to :user
+  has_many :reviews, dependent: :destroy
+  has_many :recipe_allergens, dependent: :destroy
+  has_many :allergens, through: :recipe_allergens
+
+  validates :title, presence: true
+  validates :description, presence: true
+  validates :instructions, presence: true
 end
